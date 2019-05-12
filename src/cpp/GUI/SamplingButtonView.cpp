ed3f39c1e5b321eb64e8f8abf9f478dffbebc073
Yan Pujante <ypujante@gmail.com>
2019-05-12 11:35:38 -0700

    adjusted progress bar during sampling

--- src/cpp/GUI/SamplingButtonView.cpp
+++ src/cpp/GUI/SamplingButtonView.cpp
@@ -22,6 +22,14 @@ public:
   //------------------------------------------------------------------------
   explicit SamplingButtonView(const CRect &iSize) : PluginView(iSize) {};
 
+  // get/set progress bar margin
+  Margin const &getProgressBarMargin() const { return fProgressBarMargin; }
+  void setProgressBarMargin(Margin  const &iMargin) { fProgressBarMargin = iMargin; }
+
+  // get/set progress bar color
+  const CColor &getProgressBarColor() const { return fProgressBarColor; }
+  void setProgressBarColor(const CColor &iColor) { fProgressBarColor = iColor; }
+
   //------------------------------------------------------------------------
   // draw
   //------------------------------------------------------------------------
@@ -35,7 +43,11 @@ public:
     {
       auto rdc = pongasoft::VST::GUI::RelativeDrawContext{this, iContext};
       auto width = getWidth() * percentSampled;
-      rdc.fillRect(0, 0, width, getHeight(), CColor{255,255,255,120});
+      rdc.fillRect(0 + fProgressBarMargin.fLeft,
+                   0 + fProgressBarMargin.fTop,
+                   width - fProgressBarMargin.fRight,
+                   getHeight() - fProgressBarMargin.fBottom,
+                   fProgressBarColor);
     }
   }
 
@@ -86,9 +98,26 @@ public:
     CustomViewAdapter::onParameterChange(iParamID);
   }
 
+protected:
+  Margin fProgressBarMargin{};
+  CColor fProgressBarColor{255,255,255,120};
+
 public:
-  // Creator
-  using Creator = Views::CustomViewCreator<SamplingButtonView, Views::TextButtonView>;
+  class Creator : public Views::CustomViewCreator<SamplingButtonView, Views::TextButtonView>
+  {
+  public:
+    explicit Creator(char const *iViewName = nullptr, char const *iDisplayName = nullptr) noexcept :
+      CustomViewCreator(iViewName, iDisplayName)
+    {
+      registerMarginAttribute("progress-bar-margin",
+                              &SamplingButtonView::getProgressBarMargin,
+                              &SamplingButtonView::setProgressBarMargin);
+      registerColorAttribute("progress-bar-color",
+                              &SamplingButtonView::getProgressBarColor,
+                              &SamplingButtonView::setProgressBarColor);
+    }
+
+  };
 };
 
 // the creator
