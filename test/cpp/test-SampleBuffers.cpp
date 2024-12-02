2c4741072fa3f2c798c45181645555785895a20f
Yan Pujante <yan@pongasoft.com>
2024-12-02 11:19:22 -0800

    fixed test failing on windows / release
    - failure was due to how floats are tested, not a bug in the code

--- test/cpp/test-SampleBuffers.cpp
+++ test/cpp/test-SampleBuffers.cpp
@@ -21,6 +21,16 @@ inline V32 toVector(std::unique_ptr<SampleBuffers32> const &iBuffer, int32 iChan
   return res;
 }
 
+// Helper function to compare two vectors of floats with ASSERT_FLOAT_EQ
+void ASSERT_FLOAT_VECTOR_EQ(V32 const &expected, V32 const &actual)
+{
+  ASSERT_EQ(expected.size(), actual.size()) << "Vectors are of unequal length";
+  for(size_t i = 0; i < expected.size(); ++i)
+  {
+    ASSERT_FLOAT_EQ(expected[i], actual[i]) << "Vectors differ at index " << i;
+  }
+}
+
 // SampleBuffers - computeAvg
 TEST(SampleBuffers, computeAvg)
 {
@@ -39,13 +49,13 @@ TEST(SampleBuffers, computeAvg)
   // making sure it works for bucket of size 1 -> avg is value
   ASSERT_EQ(5, sampleBuffers.computeAvg(0, avg, 0, 1, 5));
   expected = V32{1,2,3,4,5};
-  ASSERT_EQ(expected, avg);
+  ASSERT_FLOAT_VECTOR_EQ(expected, avg);
 
   // making sure we can ask for more buckets but get back only what is available
   avg.clear();
   ASSERT_EQ(2, sampleBuffers.computeAvg(0, avg, 8, 1, 5));
   expected = V32{9,10};
-  ASSERT_EQ(expected, avg);
+  ASSERT_FLOAT_VECTOR_EQ(expected, avg);
 
   // make sure it works with fractional bucket size
   avg.clear();
@@ -53,21 +63,20 @@ TEST(SampleBuffers, computeAvg)
   expected = V32{static_cast<float>((1.0 * 1.0  + 2.0 * 0.25) / 1.25),
                  static_cast<float>((2.0 * 0.75 + 3.0 * 0.5)  / 1.25),
                  static_cast<float>((3.0 * 0.5  + 4.0 * 0.75) / 1.25)};
-  ASSERT_EQ(expected, avg);
+  ASSERT_FLOAT_VECTOR_EQ(expected, avg);
 
   // make sure it works with fractional bucket size when there is less available
   avg.clear();
   ASSERT_EQ(1, sampleBuffers.computeAvg(0, avg, 7, 3.25, 2));
   expected = V32{static_cast<float>((8.0 * 1.0  + 9.0 * 1.0 + 10.0 * 1.0) / 3.0)};
-  ASSERT_EQ(expected, avg);
+  ASSERT_FLOAT_VECTOR_EQ(expected, avg);
 
   // make sure it works with fractional bucket size when there is less available
   avg.clear();
   ASSERT_EQ(2, sampleBuffers.computeAvg(0, avg, 8, 1.25, 2));
   expected = V32{static_cast<float>((9.0 * 1.0  + 10.0 * 0.25) / 1.25),
                  static_cast<float>((10.0 * 0.75) / 0.75)};
-  ASSERT_EQ(expected, avg);
-
+  ASSERT_FLOAT_VECTOR_EQ(expected, avg);
 }
 
 // SampleBuffers - cut
