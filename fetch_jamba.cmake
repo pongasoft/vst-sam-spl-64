5d60c9ea28872da3938c9d143d66e0cd607210aa
Yan Pujante <yan@pongasoft.com>
2024-12-02 10:51:58 -0800

    Use Jamba 7.1.3
    - added CI step

--- fetch_jamba.cmake
+++ fetch_jamba.cmake
@@ -3,41 +3,40 @@ cmake_minimum_required(VERSION 3.19)
 include(FetchContent)
 
 set(JAMBA_GIT_REPO "https://github.com/pongasoft/jamba" CACHE STRING "Jamba git repository url")
-set(JAMBA_GIT_TAG v7.1.2 CACHE STRING "Jamba git tag")
-set(JAMBA_DOWNLOAD_URL "${JAMBA_GIT_REPO}/archive/refs/tags/v7.1.2.zip" CACHE STRING "Jamba download url")
-set(JAMBA_DOWNLOAD_URL_HASH "SHA256=ad297edbfd6aaadea8a1762d58fff622c6e7275a3881c2d89e7b0e451ed757b2" CACHE STRING "Jamba download url hash")
+set(JAMBA_GIT_TAG v7.1.3 CACHE STRING "Jamba git tag")
+set(JAMBA_DOWNLOAD_URL "${JAMBA_GIT_REPO}/archive/refs/tags/v7.1.3.zip" CACHE STRING "Jamba download url")
+set(JAMBA_DOWNLOAD_URL_HASH "SHA256=c177c031afebf97e3c8ccaf70e9de09a1cfbe4d4503e45106612cceef03f3fe0" CACHE STRING "Jamba download url hash")
 
 if(JAMBA_ROOT_DIR)
   message(STATUS "Using jamba from local ${JAMBA_ROOT_DIR}")
-  FetchContent_Populate(jamba
-      QUIET
-      SOURCE_DIR        "${JAMBA_ROOT_DIR}"
-      BINARY_DIR        "${CMAKE_BINARY_DIR}/jamba-build"
+  FetchContent_Declare(jamba
+      SOURCE_DIR    "${JAMBA_ROOT_DIR}"
+      SOURCE_SUBDIR "do_not_make_available" # invalid folder to not execute jamba/CMakeLists.txt
   )
 else()
   if(JAMBA_DOWNLOAD_URL STREQUAL "" OR JAMBA_DOWNLOAD_URL_HASH STREQUAL "")
     message(STATUS "Fetching jamba from ${JAMBA_GIT_REPO}/tree/${JAMBA_GIT_TAG}")
-    FetchContent_Populate(jamba
-        QUIET
+    FetchContent_Declare(jamba
         GIT_REPOSITORY    ${JAMBA_GIT_REPO}
         GIT_TAG           ${JAMBA_GIT_TAG}
         GIT_CONFIG        advice.detachedHead=false
         GIT_SHALLOW       true
         SOURCE_DIR        "${CMAKE_BINARY_DIR}/jamba"
-        BINARY_DIR        "${CMAKE_BINARY_DIR}/jamba-build"
+        SOURCE_SUBDIR     "do_not_make_available"
     )
   else()
     message(STATUS "Fetching jamba from ${JAMBA_DOWNLOAD_URL}")
-    FetchContent_Populate(jamba
-        QUIET
+    FetchContent_Declare(jamba
         URL                        "${JAMBA_DOWNLOAD_URL}"
         URL_HASH                   "${JAMBA_DOWNLOAD_URL_HASH}"
         DOWNLOAD_EXTRACT_TIMESTAMP true
         SOURCE_DIR                 "${CMAKE_BINARY_DIR}/jamba"
-        BINARY_DIR                 "${CMAKE_BINARY_DIR}/jamba-build"
+        SOURCE_SUBDIR              "do_not_make_available"
     )
   endif()
 endif()
+
+FetchContent_MakeAvailable(jamba)
 
 set(JAMBA_ROOT_DIR ${jamba_SOURCE_DIR})
 
